179f6f4da8c585aa2e578f52574cf766ce42bb6c
langar <2642299437@qq.com>
2022-01-23 22:37:28 +0800

    项目建立！

---
+++ src/main/java/com/landao/hearu/HearuApplication.java
@@ -0,0 +1,13 @@
+package com.landao.hearu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearuApplication.class, args);
+    }
+
+}
