500dc8f7d538af8db2aa49b60b0815b2c7dcf362
langar <2642299437@qq.com>
2022-01-25 22:22:15 +0800

    文件上传

--- src/main/java/com/landao/hearu/controller/ApiController.java
+++ src/main/java/com/landao/hearu/controller/ApiController.java
@@ -1,7 +1,13 @@
 package com.landao.hearu.controller;
 
+import com.landao.hearu.model.common.CommonResult;
+import com.landao.hearu.safe.annotations.RequiredLogin;
+import com.landao.hearu.util.FileUploadUtil;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 公共访问接口
@@ -11,6 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
 
+    /**
+     * 文件上传
+     * @param file 文件
+     * @return 文件上传成功会返回文件地址字符串在data
+     * 返回示例/res/hearu/2022-1-25/uuid.jpg
+     * 需要前端拼接服务器ip和端口
+     */
+    @RequiredLogin
+    @PostMapping("/upload")
+    public CommonResult<String> upload(@RequestParam MultipartFile file){
+        CommonResult<String> result=new CommonResult<>();
 
+        if(file.isEmpty()){
+            return result.err("文件不能为空");
+        }
+
+        String resAddress = FileUploadUtil.uploadFile(file);
+
+        return result.body(resAddress);
+    }
 
 }
---
+++ src/main/java/com/landao/hearu/util/FileUploadUtil.java
@@ -0,0 +1,49 @@
+package com.landao.hearu.util;
+
+import com.landao.hearu.model.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ * 文件上传工具类
+ */
+@Slf4j
+public class FileUploadUtil {
+
+
+    public static String uploadFile(MultipartFile file) {
+        String originalName = file.getOriginalFilename();
+        if (StringUtils.isBlank(originalName)) {
+            throw new BusinessException("原始文件名不得为空");
+        }
+
+        String extendName = originalName.substring(originalName.lastIndexOf(".") + 1);
+        String fileName = UUID.randomUUID() + "." + extendName;
+        LocalDate now = LocalDate.now();
+        String realPath = "/home/data/hearu/" + now;
+        File dir = new File(realPath);
+        if (!dir.exists()) {
+            boolean makeDir = dir.mkdirs();
+            if(!makeDir){
+                throw new BusinessException("文件夹创建失败",-999);
+            }
+        }
+        try {
+            file.transferTo(new File(dir, fileName));
+        } catch (IOException e) {
+            log.error(JsonUtil.parse(e));
+            throw new BusinessException("文件上传失败");
+        }
+
+
+        return "/res/hearu/" + now + "/" + fileName;
+
+    }
+
+}
